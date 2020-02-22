1fb32119bf7f2e49f078b464edcee66da460338f
Francisco Belliard <frank8922@gmail.com>
2020-02-22 17:10:23 -0500

    fixed parseHost()

--- proj2/passaround.c
+++ proj2/passaround.c
@@ -201,7 +201,7 @@ void bindAddress_to_sock(struct sockaddr_in* client_addr,int *sockfd,struct host
 char* parseHost(char** msg)
 {		
 	int len_send_addr = strcspn(*msg,":"); //parse length of send address;
-	char * send_addr = (char *)malloc(len_send_addr * sizeof(char)); //allocate space for address
+	char * send_addr; //= (char *)malloc(len_send_addr * sizeof(char)); //allocate space for address
 	send_addr = strtok(*msg,":"); //parse and store send address
 	printf("DEBUG:send_addr=%s n=%d\n",send_addr,len_send_addr);
 	return send_addr;	
