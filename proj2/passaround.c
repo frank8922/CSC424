995d2e3e802b925fa2dbe6fc18c438ed52b54c96
Francisco Belliard <frank8922@gmail.com>
2020-02-22 15:16:20 -0500

    refactored code

--- proj2/passaround.c
+++ proj2/passaround.c
@@ -9,7 +9,6 @@
 **
 */
 
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
@@ -31,6 +30,124 @@
 
 #define USAGE_MESSAGE "usage: passaround [-v] [-n num] [-m message] port"
 #define PROG_NAME "passaround" 
+
+void opensocket(int *sockfd);
+void getHostname(struct hostent** host_ent, char* send_addr);
+void bindAddress_to_sock(struct sockaddr_in* client_addr,int *sockfd,struct hostent* host_ent,int port);
+char* parseHost(char** msg);
+char* parsePayload(char** msg,int len_of_msg);
+int sendPayload(int sockfd, char* payload, int len_of_payload, struct sockaddr_in* client_addr);
+
+int g_verbose = 0 ;
+
+int main(int argc, char * argv[]) 
+{
+
+ 	int sockfd ; //socket file descriptor
+	struct sockaddr_in server_addr; //holds inet protocol, address port,
+	struct sockaddr_in client_addr; //holds inet protocol, address port,
+									// ipv4 address, sin_zero(not used).
+	struct hostent *host_ent;
+	int numbytes_sent; //holds the number of bytes written out
+	int ch ;
+	int the_port = 0 ;
+	int n_repeat = N_REPEAT_DEFAULT ; //
+	char * msg = NULL ; //ponter to the msg
+	int is_forever = 0 ;
+	
+	assert(sizeof(short)==2) ; 
+
+	/* getopt()
+	 * OPTSTRING contains the option letters to be recognized; if a letter is followed by a colon, the option is expected to have an argument, which should be separated from it by white space. 
+	 * When an option requires an argument, getopts places that argument into the shell variable OPTARG. */
+	
+	while ((ch = getopt(argc, argv, "vm:n:")) != -1)
+	{
+		switch(ch) {
+		case 'n': // -n forward number packets to forward, then exit.
+			n_repeat = atoi(optarg) ; //converts args from strings to int (optarg is )
+			break ;
+		case 'v': //-v verbose
+			g_verbose = 1 ;
+			break ;
+		case 'm': //-m Take as the "message" the first received message
+			msg = strdup(optarg) ; //duplcates arguments and returrns pointer to them
+			break ;
+		case '?':
+		default:
+			printf(USAGE_MESSAGE) ;
+			return 0 ;
+		}
+	}
+	argc -= optind; //decrement the total (arg count) by the opt index
+	argv += optind; //increment the argv (array passed) by the opt index
+
+	if ( argc!= 1 ) { //if incorrect command line args, throw error
+		fprintf(stderr,"%s\n",USAGE_MESSAGE) ;
+		exit(0) ;
+	}
+
+	the_port = atoi(*argv) ; //convert to integer and store in the_port
+	printf("port: %d\n",the_port);
+	assert(the_port) ;
+
+	is_forever = (n_repeat == 0) ; //flag to terminate sending packets, 
+								   //if n_repeat is 0, loop infinitely 
+
+	//**parse first address from message and send payload
+	if ( msg ) 
+	{
+		// parse and send
+		 int len_of_msg = strlen(msg);
+		 char * send_addr;
+		 char * payload;
+
+		 send_addr = parseHost(&msg);
+		 payload = parsePayload(&msg,len_of_msg);
+
+		 //**get hostname
+		 getHostname(&host_ent,send_addr);
+		 //**end get hostname
+
+		 //**open socket
+		 opensocket(&sockfd);
+		 //**end open socket
+		
+		 //**binding address to socket
+		 bindAddress_to_sock(&client_addr,&sockfd,host_ent,the_port);
+		 //**end binding address to socket
+		
+		 //**send payload to address
+		 numbytes_sent = sendPayload(sockfd,payload,strlen(payload),&client_addr);
+		 //**end send payload to address
+
+		
+		 printf("sent %d bytes to %s\n",numbytes_sent,send_addr);
+
+		 free(msg); 
+		 free(payload);
+		 //free(send_addr);
+		 n_repeat-- ; // a packet sent
+	}
+	//**end parse first address from message and send payload
+
+		
+	while( is_forever || n_repeat ) {
+	
+		// listen for a packet
+		// print R: host:port |message|
+
+		// if something to send, {
+		//    parse and send
+		//    and print S: host:port |message|
+		// }
+
+		n_repeat-- ;
+	}
+
+	close(sockfd); //close socket
+	return 0 ;
+}
 
 /* param: pointer to socket file descriptor
  * throws error and exits if error.
@@ -84,7 +201,7 @@ void bindAddress_to_sock(struct sockaddr_in* client_addr,int *sockfd,struct host
 char* parseHost(char** msg)
 {		
 	int len_send_addr = strcspn(*msg,":"); //parse length of send address;
-	char * send_addr = (char *)malloc(len_send_addr * sizeof(char));
+	char * send_addr = (char *)malloc(len_send_addr * sizeof(char)); //allocate space for address
 	send_addr = strtok(*msg,":"); //parse and store send address
 	printf("DEBUG:send_addr=%s n=%d\n",send_addr,len_send_addr);
 	return send_addr;	
@@ -96,7 +213,7 @@ char* parseHost(char** msg)
 char* parsePayload(char** msg,int len_of_msg)
 {
 	int len_send_addr = strcspn(*msg,":"); //parse length of send address;
-	int len_of_payload = len_of_msg - len_send_addr;
+	int len_of_payload = len_of_msg - len_send_addr; //calculate the len of payload
 	char * payload = (char *)malloc(len_of_payload * sizeof(char)); //allocate space for payload
 	memset(payload,'\0',len_of_payload * sizeof(*payload)); //clear memory
 	memcpy(payload,&((*msg)[len_send_addr+1]),sizeof(char) * len_of_payload-1); //extract and copy only payload from orignal msg
@@ -119,132 +236,3 @@ int sendPayload(int sockfd, char* payload, int len_of_payload, struct sockaddr_i
 	}
 	return numbytes_sent;
 }
-
-
-int g_verbose = 0 ;
-
-int main(int argc, char * argv[]) 
-{
-
- 	int sockfd ; //socket file descriptor
-	struct sockaddr_in server_addr; //holds inet protocol, address port,
-	struct sockaddr_in client_addr; //holds inet protocol, address port,
-					// ipv4 address, sin_zero(not used)
-	struct hostent *host_ent;
-	int numbytes_sent; //holds the number of bytes written out
-	int ch ;
-	int the_port = 0 ;
-	int n_repeat = N_REPEAT_DEFAULT ; //
-	char * msg = NULL ; //ponter to the msg
-	int is_forever = 0 ;
-	
-	assert(sizeof(short)==2) ; 
-
-	/* getopt()
-	 * OPTSTRING contains the option letters to be recognized; if a letter is followed by a colon, the option is expected to have an argument, which should be separated from it by white space. 
-	 * When an option requires an argument, getopts places that argument into the shell variable OPTARG. */
-	
-while ((ch = getopt(argc, argv, "vm:n:")) != -1) {
-		switch(ch) {
-		case 'n': // -n forward number packets to forward, then exit.
-			n_repeat = atoi(optarg) ; //converts args from strings to int (optarg is )
-			break ;
-		case 'v': //-v verbose
-			g_verbose = 1 ;
-			break ;
-		case 'm': //-m Take as the "message" the first received message
-			msg = strdup(optarg) ; //duplcates arguments and returrns pointer to them
-			break ;
-		case '?':
-		default:
-			printf(USAGE_MESSAGE) ;
-			return 0 ;
-		}
-	}
-	argc -= optind; //decrement the total (arg count) by the opt index
-	argv += optind; //increment the argv (array passed) by the opt index
-
-	if ( argc!= 1 ) { //if incorrect command line args, throw error
-		fprintf(stderr,"%s\n",USAGE_MESSAGE) ;
-		exit(0) ;
-	}
-
-	the_port = atoi(*argv) ; //convert to integer and store in the_port
-	printf("port: %d\n",the_port);
-	assert(the_port) ;
-
-	is_forever = (n_repeat == 0) ; //flag to terminate sending packets, 
-								   //if n_repeat is 0, loop infinitely 
-
-	//**parse first address from message and send payload
-	if ( msg ) {
-
-		// parse and send
-		 int len_of_msg = strlen(msg);
-		 char * send_addr;
-		 char * payload;
-
-		 send_addr = parseHost(&msg);
-		 payload = parsePayload(&msg,len_of_msg);
-
-		 //**get hostname
-		 getHostname(&host_ent,send_addr);
-		 //**end get hostname
-
-		 //**open socket
-		 opensocket(&sockfd);
-		 //**end open socket
-		
-		 //**binding address to socket
-		 bindAddress_to_sock(&client_addr,&sockfd,host_ent,the_port);
-		 //**end binding address to socket
-		
-		 //**send payload to address
-		 numbytes_sent = sendPayload(sockfd,payload,strlen(payload),&client_addr);
-		 //**end send payload to address
-
-		
-		 printf("sent %d bytes to %s\n",numbytes_sent,send_addr);
-
-		 free(msg); 
-		 free(payload);
-		 //free(send_addr);
-		 n_repeat-- ; // a packet sent
-	}
-	//**end parse first address from message and send payload
-
-		
-	while( is_forever || n_repeat ) {
-	
-		// listen for a packet
-		// print R: host:port |message|
-
-		// if something to send, {
-		//    parse and send
-		//    and print S: host:port |message|
-		// }
-
-		n_repeat-- ;
-	}
-
-	close(sockfd); //close socket
-	return 0 ;
-}
-
-
-/* 	//Specifications:
-
-	*- The program listens for an incoming packet. 
-	*- When a packet is received it decodes the list of IP address. 
-	*- If the list is not empty, the program takes the first IP off the list, 
-		and forward the packet to this IP, with the packet payload being the
-		remaining addresses on the list. 
-
-
-	//Needs:
-
-		*- A function that initiates a connection with a server
-			using everything required to read and write to sockets
-		*- A function to decode packets and return ip address
-		*- A function that fowards a packet to an address (payload will be reaminig addresses)
- */
