1e97b2ac3a4cc8b2bd661c4e7eef864c67c03944
Francisco Belliard <frank8922@gmail.com>
2020-03-07 09:32:59 -0500

    added proj3

---
+++ proj3/ttftp-client.c
@@ -0,0 +1,62 @@
+/*
+** name: ttftp-client.c
+**
+** author: 
+** created:
+** last modified:
+**
+** from template created 31 jan 2015 by bjr
+**
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#include<assert.h>
+#include<unistd.h>
+
+#include "ttftp.h"
+
+#define h_addr h_addr_list[0]
+
+int ttftp_client( char * to_host, int to_port, char * file ) {
+	int block_count ; 
+	
+	/*
+	 * create a socket to send
+	 */
+	 
+	/*
+	 * send RRQ
+	 */
+
+	block_count = 1 ; /* value expected */
+	while ( block_count ) {
+	
+		/*
+		 * read at DAT packet
+		 */
+		 
+		/*
+		 * write bytes to stdout
+		 */
+		 
+		/*
+		 * send an ACK
+		 */
+
+		block_count ++ ;
+		
+		/* check if more blocks expected, else 
+		 * set block_count = 0 ;
+		 */
+	}
+	return 0 ;
+}
+
